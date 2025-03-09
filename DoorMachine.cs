54e40a470a70cbfc15f42668367c2cb7ff008ae4
aisyah <aaiisyah2007@gmail.com>
2025-03-09 18:28:55 +0700

    menambahkan implementasi dengan state-based construction

---
+++ DoorMachine.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+
+namespace tpmodul4_103022300116
+{
+    public class DoorMachine
+    {
+        private enum State { Terkunci, Terbuka}
+        private State currentState;
+
+        public DoorMachine()
+        {
+            currentState = State.Terkunci;
+            Console.WriteLine("Pintu terkunci");
+        }
+
+        public void Unlock()
+        {
+            if (currentState == State.Terkunci)
+            {
+                currentState = State.Terbuka;
+                Console.WriteLine("Pintu tidak terkunci");
+            }
+            else
+            {
+                Console.WriteLine("Pintu sudah tidak terkunci");
+            }
+        }
+
+        public void Lock()
+        {
+            if (currentState == State.Terbuka) { 
+                currentState = State.Terkunci;
+                Console.WriteLine("Pintu terkunci");
+            }
+            else
+            {
+                Console.WriteLine("Pintu sudah terkunci");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,9 +4,30 @@ using tpmodul4_103022300116;
 
 class Program
 {
+    
     static void Main(string[] args)
     {
-        Console.WriteLine("Kode Pos Batununggal: " + KodePos.getKodePos("Batununggal"));
-        Console.WriteLine("Kode Pos Samoja: " + KodePos.getKodePos("Samoja"));
+        DoorMachine door = new DoorMachine();
+
+        door.Unlock();
+        door.Lock();
+        door.Lock();
+        door.Unlock();
+        door.Unlock();
     }
+
+
+
 }
+
+        //    Console.WriteLine("Kode Pos Batununggal: " + KodePos.getKodePos("Batununggal"));
+        //    Console.WriteLine("Kode Pos Kujangsari: " + KodePos.getKodePos("Kujangsari"));
+        //    Console.WriteLine("Kode Pos Mengger: " + KodePos.getKodePos("Mengger"));
+        //    Console.WriteLine("Kode Pos Wates: " + KodePos.getKodePos("Wates"));
+        //    Console.WriteLine("Kode Pos Cijaura: " + KodePos.getKodePos("Cijaura"));
+        //    Console.WriteLine("Kode Pos Jatisari: " + KodePos.getKodePos("Jatisari"));
+        //    Console.WriteLine("Kode Pos Margasari: " + KodePos.getKodePos("Margasari"));
+        //    Console.WriteLine("Kode Pos Sekejati: " + KodePos.getKodePos("Sekejati"));
+        //    Console.WriteLine("Kode Pos Kebonwaru: " + KodePos.getKodePos("Kebonwaru"));
+        //    Console.WriteLine("Kode Pos Maleer: " + KodePos.getKodePos("Maleer"));
+        //    Console.WriteLine("Kode Pos Samoja: " + KodePos.getKodePos("Samoja"));
